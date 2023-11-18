e42a4ff9859697950ade850ed4c603478ee0b1a6
Amir Shiri <amirsh@cynet.com>
2023-11-18 22:51:37 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import constants
+import sys
+from pathlib import Path
+from openai import OpenAI
+
+os.environ['OPENAI_API_KEY'] = constants.APIKEY
+text = sys.argv[1]
+
+client = OpenAI()
+
+
+# Check if the directory does not exist
+if not os.path.exists('audio'):
+  # Create the directory
+  os.mkdir('audio')
+
+speech_file_path = Path(__file__).parent / "audio" / "speech.mp3"
+response = client.audio.speech.create(
+  model="tts-1",
+  voice="shimmer",
+  input=text
+)
+response.stream_to_file(speech_file_path)
---
+++ readme.md
@@ -0,0 +1,12 @@
+for setup, run the following commands:
+
+```fish
+python3 -m venv venv
+source venv/bin/activate.fish
+pip install -r requirements.txt
+```
+update your api key in constants.py and then run
+
+```fish
+python main.py "your search query"
+```
